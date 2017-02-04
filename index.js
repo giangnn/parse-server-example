c325d9fbb44ca5e84c4b1b07d873482bffb503f3
David Nguyen <david.nguyen@leap.com.au>
2017-02-04 14:54:21 +1100

    use .NET client instead of cloud code

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ let app = express();
 
 let api = new ParseServer({
   databaseURI: 'mongodb://localhost:27017/dev', // Connection string for your MongoDB database
-  cloud: './main.js', // Absolute path to your Cloud Code
+  //cloud: './main.js', // Absolute path to your Cloud Code
   appId: 'myAppId',
   masterKey: 'myMasterKey', // Keep this key secret!
+  dotNetKey: 'myDotnetKey',
   fileKey: 'optionalFileKey',
   serverURL: 'http://localhost:1337/parse', // Don't forget to change to https if needed
   liveQuery: {
